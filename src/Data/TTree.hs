2a424a3325bb50d983628b9fe33171a330dee89d
Chris Pollard <cspollard@gmail.com>
2016-08-29 09:20:04 +0100

    not sure this is going anywhere.

--- src/Data/TTree.hs
+++ src/Data/TTree.hs
@@ -21,7 +21,7 @@ import Foreign.C.String
 
 import Control.Monad ((>=>))
 import Control.Monad.Primitive (RealWorld)
-import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Cont
 import Control.Monad.Trans.Maybe
 
 import Data.Vector.Storable hiding ((++))
@@ -59,22 +59,6 @@ foreign import ccall "ttreeC.h vectorDataD" vectorDataD
     :: VecPtr Double -> Ptr Double
 
 
-type TTree = ForeignPtr ()
-
-
-ttree :: String -> String -> IO TTree
-ttree tn fn = do tn' <- newCString tn
-                 fn' <- newCString fn
-                 tp <- newForeignPtr _ttreeFree =<< _ttree tn' fn'
-                 free tn' >> free fn'
-                 return tp
-
-
-
--- getEntry :: MonadIO m => a -> TTreeRead m (Maybe a)
--- getEntry x = do (cp, i, _) <- get
-                -- n <- liftIO $ withForeignPtr cp $ flip _ttreeGetEntry i
-                -- return $ if n > 0 then Just x else Nothing
 
 
 class Branchable b where
@@ -146,23 +130,66 @@ instance Vecable a => Branchable (ZipList a) where
     fromBranch = peek >=> toV >=> return . ZipList . toList
 
 
-type TTreeRead m a = ReaderT (TTree, Int) (MaybeT m) a
 
-readBranch :: (MonadIO m, Branchable a, Storable (HeapType a)) => String -> TTreeRead m a
-readBranch s = do (cp, i) <- ask
-                  bp <- liftIO calloc
-                  n <- liftIO $ withCString s $ \s' -> withForeignPtr cp $ \cp' -> _ttreeGetBranchEntry cp' s' i bp
-                  if n <= 0
-                     then liftIO (free bp) >> fail ("failed to read branch " ++ s)
-                     else liftIO $ fromBranch bp <* free bp
+data TTree = TTree { _tptr :: ForeignPtr ()
+                   , _tbrs :: HashMap String (ForeignPtr ())
+                   } deriving Show
+
+
+ttree :: String -> String -> IO TTree
+ttree tn fn = do tn' <- newCString tn
+                 fn' <- newCString fn
+                 tp <- newForeignPtr _ttreeFree =<< _ttree tn' fn'
+                 free tn' >> free fn'
+                 return $ TTree tp mempty
+
+
+
+-- ContT r m a :: (a -> m r) -> m r
+-- you give me a way to get from TTree to x;
+-- I'll give you x.
+type TreeRead m r = ContT r m TTree
+
+-- you give me a way to get from VPtr to x;
+-- I'll give you x.
+type BranchRead m r = ContT r m VPtr
 
 
 class FromTTree a where
-    fromTTree :: MonadIO m => TTreeRead m a
+    fromTTree :: MonadIO m => TreeRead m a
 
-class FromTTreeV a where
-    fromTTreeV :: MonadIO m => Int -> TTreeRead m a
 
+-- I don't yet know where the branch's pointer is
+-- I will only get it after GetEntry
+-- If you give me the function to access the pointer, then I will
+-- interpret it with type a.
+-- readBranch :: (MonadIO m, Branchable a, Storable (HeapType a)) => String -> BranchRead m a
+-- readBranch s = do p <- liftIO $ _tchainGetBranchAddress s
+                  -- fromBranch p
+
+-- TODO!!!!
+-- callback: give me a string and I'll give you the pointer.
+readBranch :: MonadIO m => TTree -> String -> IO (ForeignPtr a)
+readBranch t s = do p <- _tchainGetBranchAddress t s
+                    if p == nullPtr
+                       then newForeignPtr finalizerFree calloc
+                       else newForeignPtr_ p
+
+getEntry :: MonadIO m => TTree -> Int -> ContT a m TTree
+getEntry tp i f = do bs <- needsBranches f
+                     fillBranches tp i bs
+                     fillF f
+
+
+{-
+                                n <- liftIO $ withCString s $ \s' -> withForeignPtr tp $ \tp' -> _ttreeGetBranchEntry tp' s' bp
+                                if n <= 0
+                                   then liftIO (free bp) >> fail ("failed to read branch " ++ s)
+                                   else liftIO $ fromBranch bp <* free bp
+
+-}
+
+{-
 
 runTTree :: Monad m => TTreeRead (ConduitM i o m) o -> TTree -> ConduitM i o m ()
 runTTree f c = loop c 0 
@@ -178,3 +205,4 @@ runTTreeN n f c = runTTree f c =$= takeC n
 
 project :: (MonadIO m, FromTTree fc) => TTree -> ConduitM i fc m ()
 project = runTTree fromTTree
+-}
