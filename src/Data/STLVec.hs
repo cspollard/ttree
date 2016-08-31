a97223027b4dc2f542ca4c6c66aecedd0fb5b875
Chris Pollard <cspollard@gmail.com>
2016-09-01 00:22:25 +0100

    forgot these files.

---
+++ src/Data/STLVec.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE CPP #-}
+
+module Data.STLVec where
+
+import Foreign hiding (void)
+
+import qualified Data.Vector.Storable as VS
+
+foreign import ccall "ttreeC.h vectorSizeC" vectorSizeC
+    :: VecPtr Char -> Int
+foreign import ccall "ttreeC.h vectorDataC" vectorDataC
+    :: VecPtr Char -> Ptr Char
+foreign import ccall "ttreeC.h &vectorFreeC" vectorFreeC
+    :: FunPtr (Ptr (VecPtr Char) -> IO ())
+
+foreign import ccall "ttreeC.h vectorSizeI" vectorSizeI
+    :: VecPtr Int -> Int
+foreign import ccall "ttreeC.h vectorDataI" vectorDataI
+    :: VecPtr Int -> Ptr Int
+foreign import ccall "ttreeC.h &vectorFreeI" vectorFreeI
+    :: FunPtr (Ptr (VecPtr Int) -> IO ())
+
+foreign import ccall "ttreeC.h vectorSizeF" vectorSizeF
+    :: VecPtr Float -> Int
+foreign import ccall "ttreeC.h vectorDataF" vectorDataF
+    :: VecPtr Float -> Ptr Float
+foreign import ccall "ttreeC.h &vectorFreeF" vectorFreeF
+    :: FunPtr (Ptr (VecPtr Float) -> IO ())
+
+foreign import ccall "ttreeC.h vectorSizeD" vectorSizeD
+    :: VecPtr Double -> Int
+foreign import ccall "ttreeC.h vectorDataD" vectorDataD
+    :: VecPtr Double -> Ptr Double
+foreign import ccall "ttreeC.h &vectorFreeD" vectorFreeD
+    :: FunPtr (Ptr (VecPtr Double) -> IO ())
+
+
+-- pointer to a c++ vector
+newtype VecPtr a = VecPtr { vecPtr :: Ptr () } deriving (Show, Storable)
+
+class Storable a => Vecable a where
+    sizeV :: VecPtr a -> Int
+    dataV :: VecPtr a -> Ptr a
+    freeV :: FunPtr (Ptr (VecPtr a) -> IO ())
+
+    toV :: VecPtr a -> IO (VS.Vector a)
+    toV vp = flip VS.unsafeFromForeignPtr0 (sizeV vp) <$> newForeignPtr_ (dataV vp)
+
+
+instance Vecable Char where
+    sizeV = vectorSizeC
+    dataV = vectorDataC
+    freeV = vectorFreeC
+
+instance Vecable Int where
+    sizeV = vectorSizeI
+    dataV = vectorDataI
+    freeV = vectorFreeI
+
+instance Vecable Float where
+    sizeV = vectorSizeF
+    dataV = vectorDataF
+    freeV = vectorFreeF
+
+instance Vecable Double where
+    sizeV = vectorSizeD
+    dataV = vectorDataD
+    freeV = vectorFreeD
---
+++ src/Data/TBranch.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE CPP #-}
+
+module Data.TBranch ( Branchable(..), Freeable(..) ) where
+
+import Foreign hiding (void)
+
+import Control.Monad ((>=>))
+
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector as V
+
+import Control.Applicative (ZipList(..))
+
+import Data.STLVec
+
+class Branchable b where
+    type HeapType b :: *
+    fromB :: Ptr (HeapType b) -> IO b
+
+
+instance Branchable Char where
+    type HeapType Char = Char
+    fromB = peek
+
+instance Branchable Int where
+    type HeapType Int = Int
+    fromB = peek
+
+instance Branchable Float where
+    type HeapType Float = Float
+    fromB = peek
+
+instance Branchable Double where
+    type HeapType Double = Double
+    fromB = peek
+
+instance Vecable a => Branchable (V.Vector a) where
+    type HeapType (V.Vector a) = VecPtr a
+    fromB = fmap VS.convert <$> (peek >=> toV)
+
+instance Vecable a => Branchable [a] where
+    type HeapType [a] = VecPtr a
+    fromB = fmap VS.toList <$> (peek >=> toV)
+
+instance Vecable a => Branchable (ZipList a) where
+    type HeapType (ZipList a) = VecPtr a
+    fromB = fmap ZipList <$> fromB
+
+
+class Freeable a where
+    free' :: FunPtr (Ptr a -> IO ())
+
+instance Freeable Char where
+    free' = finalizerFree
+
+instance Freeable Int where
+    free' = finalizerFree
+
+instance Freeable Float where
+    free' = finalizerFree
+
+instance Freeable Double where
+    free' = finalizerFree
+
+instance Vecable a => Freeable (VecPtr a) where
+    free' = freeV
