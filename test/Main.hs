9c5527be9fb4f6a25b0e912dd514b9f9c9ca810c
Chris Pollard <cspollard@gmail.com>
2016-10-11 14:16:19 +0100

    checking vector<vector<int> >

--- test/Main.hs
+++ test/Main.hs
@@ -7,7 +7,7 @@ import Data.Vector (Vector)
 
 import Data.TTree
 
-data Event = Event CInt CInt Float (Vector Float) (Vector Float) (Vector Float) (VVector Double) deriving Show
+data Event = Event CInt CInt Float (Vector Float) (Vector Float) (Vector Float) (VVector Double) (VVector CInt) deriving Show
 
 instance FromTTree Event where
     fromTTree = Event <$> readBranch "Run"
@@ -17,6 +17,7 @@ instance FromTTree Event where
                       <*> readBranch "JetEta"
                       <*> readBranch "JetPhi"
                       <*> readBranch "JetTracksPt"
+                      <*> readBranch "JetTracksisTight"
 
 main :: IO ()
 main = do (tn:fns) <- getArgs
