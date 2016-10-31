c318233c48907a10db27e97a5e012d3694217cac
Chris Pollard <cspollard@gmail.com>
2016-10-31 11:44:56 +0000

    these fns should return IO x

--- src/Data/TH1.hs
+++ src/Data/TH1.hs
@@ -11,23 +11,23 @@ type TH1D = Ptr ()
 type TH1F = Ptr ()
 
 foreign import ccall "th1C.h th1D" th1d
-    :: CString -> CString -> TH1D
+    :: CString -> CString -> IO TH1D
 foreign import ccall "th1C.h nbinsD" nbinsd
-    :: TH1D -> CUInt
+    :: TH1D -> IO CUInt
 foreign import ccall "th1C.h binedgeD" binedged
-    :: TH1D -> CUInt -> Double
+    :: TH1D -> CUInt -> IO Double
 foreign import ccall "th1C.h entryD" entryd
-    :: TH1D -> CUInt -> Double
+    :: TH1D -> CUInt -> IO Double
 foreign import ccall "th1C.h uncertD" uncertd
-    :: TH1D -> CUInt -> Double
+    :: TH1D -> CUInt -> IO Double
 
 foreign import ccall "th1C.h th1F" th1f
-    :: CString -> CString -> TH1F
+    :: CString -> CString -> IO TH1F
 foreign import ccall "th1C.h nbinsF" nbinsf
-    :: TH1F -> CUInt
+    :: TH1F -> IO CUInt
 foreign import ccall "th1C.h binedgeF" binedgef
-    :: TH1F -> CUInt -> Double
+    :: TH1F -> CUInt -> IO Double
 foreign import ccall "th1C.h entryF" entryf
-    :: TH1F -> CUInt -> Double
+    :: TH1F -> CUInt -> IO Double
 foreign import ccall "th1C.h uncertF" uncertf
-    :: TH1F -> CUInt -> Double
+    :: TH1F -> CUInt -> IO Double
