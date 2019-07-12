ece62dd69ac898fb64b6c27e2c06148f09513700
Chris <cspollard@gmail.com>
2019-07-12 10:28:17 +0200

    adding documentation

--- test/Main.hs
+++ test/Main.hs
@@ -39,28 +39,18 @@ import Foreign
 
 
 
-type Vars = Kleisli []
+-- define some convenience types
+type Vars = Kleisli [] -- list-based syst variations
+type SFs = Kleisli ((,) (Product Float)) -- multiplicative SFs
 
-type SFs = Kleisli ((,) (Product Float))
+type ReadScal = L String BS -- can read scalar branches
+type ReadVec = L String BV -- can read vector<> branches
+type ReadVec2 = L String BV2 -- can read vector<vector<>> branches
 
-
-type ReadScal = L String BS
-type ReadVec = L String BV
-type ReadVec2 = L String BV2
-
-
-injK :: Member rels (Kleisli m) => (a -> m b) -> Analysis rels a b
-injK = liftFree <<< inj <<< Kleisli
+type f ~> g = forall x. f x -> g x -- natural transformation
 
 
-sf :: Member rels SFs => (a -> Float) -> Analysis rels a ()
-sf f = injK $ \x -> (Product $ f x, ())
-
-
-vars :: Member rels Vars => (a -> [b]) -> Analysis rels a b
-vars f = injK f
-
-
+-- reads in a variable "mu", which incurs a scale factor and some syst variations
 readMu :: Members rels '[ ReadScal, SFs, Vars ]  => Analysis rels () Float
 readMu = proc store -> do
   mu <- scalar "mu" -< store
@@ -73,6 +63,9 @@ readMu = proc store -> do
 
 
 
+-- a toy Analysis
+-- it supports reading scalar and vector values from a tree
+-- as well as systematic variations and SFs
 ana
   :: Members rels '[ ReadScal, ReadVec, Vars , SFs ]
   => Analysis rels () (Float, Vector Float)
@@ -90,6 +83,108 @@ ana = proc store -> do
 runFree' :: (Category q, Traversing q) => Free p a b -> (p :-> q) -> q a b
 runFree' a f = runFree f a
 
+liftC2 :: Applicative m => n ~> (Compose m n)
+liftC2 = Compose <<< pure
+
+liftC1 :: (Functor m, Applicative n) => m ~> Compose m n
+liftC1 = Compose <<< fmap pure
+
+
+liftK :: (m ~> n) -> Kleisli m :-> Kleisli n
+liftK nat (Kleisli f) = Kleisli $ nat <<< f
+
+
+-- a physics object: IO actions, syst variations, multiplicative SFs
+type PO = Compose IO (Compose [] ((,) (Product Float)))
+
+-- a physics analysis from a to b: a -> PO b
+type Ana = Kleisli PO
+
+
+-- some convenient map types
+type MHM = HM.MonoidalHashMap String
+type MHM' = MHM (First (IO (ForeignPtr ())))
+
+
+main ::IO ()
+main = do
+  (fname : tname : _) <- getArgs
+
+  -- open the TFile and grab the TTree
+  f <- tfileOpen fname
+
+  t <- ttree f tname
+
+  -- this section builds the list of branches that need to be read in.
+  let cnv = mapConst (\(s, p) -> HM.singleton s (First p))
+      Const hm =
+        runFree' ana
+        $ extract @(Const MHM')
+          <<< runU (inj <<< ignVars @MHM') -- ignore syst variations 
+          <<< runU (inj <<< ignSFs @MHM') -- ignore SFs
+          <<< runU (inj <<< cnv <<< ptrBS) -- keep scalar branch names
+          <<< runU (inj <<< cnv <<< ptrBV) -- keep vector branch names
+          <<< runU (inj <<< cnv <<< ptrBV2) -- keep vector2 branch names
+
+
+  -- a map of Sting to Ptrs for the branches
+  hm' <- traverse getFirst (hm::MHM')
+
+  -- branches connected to the tree!
+  _ <- either error id <$> connectBranches t hm'
+
+
+  let lookup' h k = maybe (error $ "missing " ++ k) id $ HM.lookup k h
+      hook = lookup' hm'
+  
+      -- "compile" the analysis to something that can actually be run on the CPU
+      (prog :: Ana () (Float, Vector Float)) =
+        runFree' ana
+        $ extract
+          <<< runU (inj <<< handleVars) -- lift Vars to a physics object
+          <<< runU (inj <<< handleSFs) -- lift SFs to a physics object
+          <<< runU (inj <<< handleIO) -- lift IO to a physics object
+          <<< runU (inj <<< readBS <<< mapL hook) -- read scalar branches
+          <<< runU (inj <<< readBV <<< mapL hook) -- read vector branches
+          <<< runU (inj <<< readBV2 <<< mapL hook) -- read vector2 branches
+
+
+  -- read in entry zero and 100 and print the results of the analysis
+  _ <- loadEntry t 0
+  
+  print =<< getCompose (runKleisli prog ())
+
+  _ <- loadEntry t 100
+  
+  print =<< getCompose (runKleisli prog ())
+
+
+-- physcis objects are Monads
+class Swap m n where
+  swap :: n (m a) -> m (n a)
+
+instance (Monad m, Monad n, Swap m n) => Monad (Compose m n) where
+  Compose mna >>= f =
+    let mnmnb = fmap (getCompose . f) <$> mna
+        mnb = join $ fmap join . swap <$> mnmnb
+    in Compose mnb
+
+instance (Applicative m, Traversable t) => Swap m t where
+  swap = sequenceA
+
+
+injK :: Member rels (Kleisli m) => (a -> m b) -> Analysis rels a b
+injK = liftFree <<< inj <<< Kleisli
+
+
+sf :: Member rels SFs => (a -> Float) -> Analysis rels a ()
+sf f = injK $ \x -> (Product $ f x, ())
+
+
+vars :: Member rels Vars => (a -> [b]) -> Analysis rels a b
+vars f = injK f
+
+
 toConst :: Monoid m => p :-> Const m
 toConst _ = Const mempty
 
@@ -100,26 +195,8 @@ ignSFs :: Monoid m => SFs :-> Const m
 ignSFs = toConst
 
 
-
-liftC2 :: Applicative m => n ~> (Compose m n)
-liftC2 = Compose <<< pure
-
-liftC1 :: (Functor m, Applicative n) => m ~> Compose m n
-liftC1 = Compose <<< fmap pure
-
-
-type f ~> g = forall x. f x -> g x
-
-
-liftK :: (m ~> n) -> Kleisli m :-> Kleisli n
-liftK nat (Kleisli f) = Kleisli $ nat <<< f
-
-
-type KIO = Kleisli IO
-type PO = Compose IO (Compose [] ((,) (Product Float)))
-type Ana = Kleisli PO
-
-handleIO :: KIO :-> Ana
+-- the following lift relations in IO, Vars, SFs to relations in physics objects
+handleIO :: Kleisli IO :-> Ana
 handleIO = liftK liftC1
 
 handleVars :: Vars :-> Ana
@@ -133,64 +210,3 @@ handleSFs = liftK (liftC2 <<< liftC2)
 getBranches :: Const MHM' a b -> MHM'
 getBranches = getConst
 
-
-type MHM = HM.MonoidalHashMap String
-type MHM' = MHM (First (IO (ForeignPtr ())))
-
-main ::IO ()
-main = do
-  (fname : tname : _) <- getArgs
-
-  f <- tfileOpen fname
-
-  t <- ttree f tname
-
-  let cnv = mapConst (\(s, p) -> HM.singleton s (First p))
-      Const hm =
-        runFree' ana
-        $ extract @(Const MHM')
-          <<< runU (inj <<< ignVars @MHM')
-          <<< runU (inj <<< ignSFs @MHM')
-          <<< runU (inj <<< cnv <<< ptrBS)
-          <<< runU (inj <<< cnv <<< ptrBV)
-          <<< runU (inj <<< cnv <<< ptrBV2)
-
-
-  hm' <- traverse getFirst (hm::MHM')
-
-  _ <- either error id <$> connectBranches t hm'
-
-  let lookup' h k = maybe (error $ "missing " ++ k) id $ HM.lookup k h
-      hook = lookup' hm'
-  
-      (prog :: Ana () (Float, Vector Float)) =
-        runFree' ana
-        $ extract
-          <<< runU (inj <<< handleVars)
-          <<< runU (inj <<< handleSFs)
-          <<< runU (inj <<< handleIO)
-          <<< runU (inj <<< readBS <<< mapL hook)
-          <<< runU (inj <<< readBV <<< mapL hook)
-          <<< runU (inj <<< readBV2 <<< mapL hook)
-
-
-  print =<< loadEntry t 0
-  
-  print =<< getCompose (runKleisli prog ())
-
-  print =<< loadEntry t 100
-  
-  print =<< getCompose (runKleisli prog ())
-
-
-class Swap m n where
-  swap :: n (m a) -> m (n a)
-
-instance (Monad m, Monad n, Swap m n) => Monad (Compose m n) where
-  Compose mna >>= f =
-    let mnmnb = fmap (getCompose . f) <$> mna
-        mnb = join $ fmap join . swap <$> mnmnb
-    in Compose mnb
-
-instance (Applicative m, Traversable t) => Swap m t where
-  swap = sequenceA
