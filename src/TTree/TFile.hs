4dc7a6c157c0a3fcc46cf3fc588941771db0a96b
Chris <cspollard@gmail.com>
2019-07-12 10:57:55 +0200

    some more spring cleaning

--- src/TTree/TFile.hs
+++ src/TTree/TFile.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module TTree.TFile
-  ( module X
-  , TFile(..)
+  ( TFile(..)
   , tfileOpen, tfileClose, tfileGet
   ) where
 
 
-import           Control.Monad.IO.Class as X (MonadIO (..))
 import           Foreign                hiding (void)
 import           Foreign.C.String
 
@@ -22,11 +20,11 @@ foreign import ccall "tfileC.h tfileGet" _tfileGet
     :: TFile -> CString -> IO (Ptr ())
 
 
-tfileOpen :: MonadIO m => String -> m TFile
-tfileOpen = liftIO . flip withCString _tfileOpen
+tfileOpen :: String -> IO TFile
+tfileOpen = flip withCString _tfileOpen
 
-tfileGet :: MonadIO m => TFile -> String -> m (Ptr ())
-tfileGet tf s = liftIO . withCString s $ _tfileGet tf
+tfileGet :: TFile -> String -> IO (Ptr ())
+tfileGet tf s = withCString s $ _tfileGet tf
 
-tfileClose :: MonadIO m => TFile -> m ()
-tfileClose = liftIO . _tfileClose
+tfileClose :: TFile -> IO ()
+tfileClose = _tfileClose
