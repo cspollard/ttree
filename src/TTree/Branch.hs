758c6971133cfed18bb7cc89b7c86aa80bbf718b
Chris <cspollard@gmail.com>
2019-07-12 10:55:37 +0200

    inlines

--- src/TTree/Branch.hs
+++ src/TTree/Branch.hs
@@ -17,18 +17,21 @@ scalar
   :: (Member arrs (L s BS), Storable a)
   => s -> Analysis arrs () a
 scalar s = liftFree <<< inj $ L s BS
+{-# INLINE scalar  #-}
 
 
 vector
   :: (Member arrs (L s BV), Vecable a)
   => s -> Analysis arrs () (Vector a)
 vector s = liftFree <<< inj $ L s BV
+{-# INLINE vector  #-}
 
 
 vector2
   :: (Member arrs (L s BV2), Vecable2 a)
   => s -> Analysis arrs () (Vector (Vector a))
 vector2 s = liftFree <<< inj $ L s BV2
+{-# INLINE vector2  #-}
 
 
 -- a labeled relation
@@ -37,10 +40,12 @@ data L s p a b = L !s !(p a b)
 
 mapL :: (s -> s') -> L s p :-> L s' p
 mapL f (L s p) = L (f s) p
+{-# INLINE mapL  #-}
 
 
 labeledToConst :: L s p :-> Const s
 labeledToConst (L s _) = Const s
+{-# INLINE labeledToConst  #-}
 
 
 data BS a b where
@@ -57,6 +62,7 @@ readBS :: L VFP BS :-> Kleisli IO
 readBS (L p BS) =
   Kleisli $ \() ->
     withForeignPtr p $ peek <<< castPtr
+{-# INLINE readBS  #-}
 
 
 readBV :: L VFP BV :-> Kleisli IO
@@ -65,6 +71,7 @@ readBV (L p BV) =
     withForeignPtr p
     $ (castPtr >>> peek)
       >=> (VecPtr >>> vecFromPtr)
+{-# INLINE readBV  #-}
 
 
 readBV2 :: L VFP BV2 :-> Kleisli IO
@@ -75,10 +82,12 @@ readBV2 (L p BV2) =
       >=> (VVecPtr >>> readVV)
       >=> vecFromPtr
       >=> traverse vecFromPtr
+{-# INLINE readBV2  #-}
 
 
 vecFromPtr :: Vecable a => VecPtr a -> IO (Vector a)
 vecFromPtr = toV >>> fmap convert
+{-# INLINE vecFromPtr  #-}
 
 
 -- for this one I need access to the type b, so I can't use :-> notation.
@@ -87,15 +96,19 @@ ptrBS (L s BS) =
   -- we take responsibility for freeing scalars.
   let p = castForeignPtr <$> mallocForeignPtr @b
   in Const (s, p)
+{-# INLINE ptrBS  #-}
 
 
 vptr :: IO VFP
 vptr = castForeignPtr <$> mallocForeignPtr @(Ptr ())
+{-# INLINE vptr  #-}
 
 
 ptrBV :: L String BV :-> Const (String, IO VFP)
 ptrBV = mapL (,vptr) >>> labeledToConst
+{-# INLINE ptrBV  #-}
 
 
 ptrBV2 :: L String BV2 :-> Const (String, IO VFP)
 ptrBV2 = mapL (,vptr) >>> labeledToConst
+{-# INLINE ptrBV2  #-}
--- src/TTree/TTree.hs
+++ src/TTree/TTree.hs
@@ -13,10 +13,6 @@ import qualified Data.HashMap.Monoidal as HM
 import TTree.Internal.Common
 import Data.Functor.Compose
 import Data.Foldable (traverse_)
-import Data.Semigroup (First(..))
-import TTree.Branch
-import Analysis.Const
-import Analysis.Free
 
 
 type HM = HM.MonoidalHashMap
@@ -43,35 +39,13 @@ connectBranch (TTree ttreeptr) k p =
       case n of
         0 -> return <<< Left $ "missing branch \"" ++ k ++ "\""
         _ -> return $ Right ()
+{-# INLINE connectBranch  #-}
 
 
 connectBranches :: TTree -> HM String VFP -> IO (Either String ())
 connectBranches t hm =
   getCompose $ traverse_ (\(k, p) -> Compose $ connectBranch t k p) (HM.toList hm)
-
-
--- interpretBranches
---   :: Member arrs (Const (HM String (First (IO VFP))))
---   => U (L String BV2 ': L String BV ': L String BS ': arrs)
---   :-> U arrs
--- interpretBranches =
---   runU (inj <<< cnv <<< ptrBS)
---   <<< runU (inj <<< cnv <<< ptrBV)
---   <<< runU (inj <<< cnv <<< ptrBV2)
---   where
---     cnv :: Const (String, IO VFP) :-> Const (HM.MonoidalHashMap String (First (IO VFP)))
---     cnv = mapConst (\(s, p) -> HM.singleton s (First p))
-
-
--- compileBranches
---   :: Member arrs (Kleisli IO)
---   => (s -> VFP)
---   -> U (L s BV2 ': L s BV ': L s BS ': arrs)
---   :-> U arrs
--- compileBranches hook =
---   runU (inj <<< readBS <<< mapL hook)
---   <<< runU (inj <<< readBV <<< mapL hook)
---   <<< runU (inj <<< readBV2 <<< mapL hook)
+{-# INLINE connectBranches  #-}
 
 
 ttree :: TFile -> String -> IO TTree
@@ -86,85 +60,3 @@ isNullTree (TTree p) = p == nullPtr
 
 loadEntry :: TTree -> Int -> IO Bool
 loadEntry (TTree p) i = (>= 0) <$> _ttreeLoadTree p i
-
-
-
--- TODO
--- -- this really should be of type Int -> TTree -> (ExceptT TreeError m) a
--- -- but this is isomorphic, no?
--- type TreeRead m = ReaderT Int (StateT TTree m)
--- 
--- 
--- readEntry :: Monad m => TreeRead m Int
--- readEntry = ask
--- 
--- 
--- -- note: this assumes that once a branch has been requested
--- -- that we want to load it for *EVERY EVENT*
--- readBranchMaybe
---   :: (MonadIO m, Branchable a, Storable (HeapType a))
---   => String -> TreeRead m (Maybe a)
--- readBranchMaybe s = do
---   i <- ask
---   t <- get
---   case s `HM.lookup` ttreeBranches t of
---     -- we've already read this branch at least once: don't alloc a
---     -- new pointer
---     Just p  -> fmap Just . liftIO $ withForeignPtr (castForeignPtr p) fromB
--- 
---     -- this is the first time we've accessed this branch: alloc a
---     -- new pointer
---     Nothing -> do
---       p <- liftIO $ newForeignPtr_ =<< calloc
---       modify . const
---         $ t { ttreeBranches = HM.insert s (castForeignPtr p) (ttreeBranches t) }
---       n <-
---         liftIO
---           $ withCString s
---           $ \s' -> withForeignPtr (ttreePtr t)
---           $ \tp' -> withForeignPtr p
---           $ \p' -> _ttreeGetBranchEntry tp' s' i p'
--- 
---       if n > 0
---          then fmap Just . liftIO $ withForeignPtr p fromB
---          else return Nothing
--- 
--- 
--- data TTreeException = EndOfTTree | TBranchReadFailure String
---   deriving (Typeable, Show)
--- 
--- instance Exception TTreeException where
--- 
--- 
--- readBranch
---   :: ( MonadIO m, MonadThrow m
---      , Branchable a, Storable (HeapType a))
---   => String -> TreeRead m a
--- readBranch s = do
---   m <- readBranchMaybe s
---   case m of
---     Nothing -> do
---       i <- ask
---       throwM . TBranchReadFailure
---         $ "unable to read branch " ++ s ++ " for event number " ++ show i
---     Just x  -> return x
--- 
--- 
--- class FromTTree a where
---     fromTTree :: (MonadIO m, MonadThrow m) => TreeRead m a
--- 
--- 
--- 
--- 
--- pipeTTree
---   :: (MonadIO m, MonadState TTree m)
---   => ReaderT Int m b
---   -> Pipe Int b m ()
--- pipeTTree f = do
---   i <- await
---   mx <- lift $ readTTreeEntry f i
---   case mx of
---     Just x -> do
---       yield x
---       pipeTTree f
---     Nothing -> return ()
